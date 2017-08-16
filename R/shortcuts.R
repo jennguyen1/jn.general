8d0b5629e7a53257283abc2e7039085c0794ad2f
Jenny <jennifernguyen1992@gmail.com>
2017-08-16 01:04:22 -0400

    migrate lib over to scriptR

--- R/shortcuts.R
+++ R/shortcuts.R
@@ -9,8 +9,8 @@
 #' install_jn() installs the jn.general package \cr
 #'
 #' lib() imports libraries depending on args: \cr
-#'  data: (data wrangling) magrittr, stringr, plyr, dplyr, tidyr, reshape2, data.table, etc \cr
-#'  viz: (data visualization) knitr, ggplot2, grid, gridExtra, GGally, gtable \cr
+#'  data: (data wrangling) magrittr, stringr, tidyverse, data.table, etc \cr
+#'  viz: (data visualization) ggplot2 and extensions \cr
 #'  model: (modeling) broom, lme4, glmnet, caret \cr
 #'  develop: (development) optparse, logging \cr
 #'
@@ -30,55 +30,4 @@ install_jn <- function() devtools::install_github("jnguyen92/jn.general")
 
 #' @rdname shortcuts
 #' @export
-lib <- function(...){
-
-  args <- as.character(substitute(list(...))[-1])
-
-  lib_opts <- data.table::data.table(data = TRUE, viz = FALSE, model = FALSE, develop = FALSE) %>% .[, (args) := TRUE]
-
-  suppressWarnings( suppressPackageStartupMessages( import_lib(lib_opts) ) )
-}
-
-import_lib <- function(lib_opts){
-
-  if(lib_opts$data){
-    # data wrangling
-    library(magrittr)
-    library(stringr)
-    library(tidyverse)
-    library(purrrlyr)
-    library(data.table)
-    library(dtplyr)
-  }
-
-  if(lib_opts$viz){
-    # data visualization
-    library(knitr)
-    library(ggplot2)
-    library(grid)
-    library(gridExtra)
-    library(GGally)
-    library(gtable)
-
-    # set ggplot2 theme
-    theme_set(theme_bw())
-  }
-
-  if(lib_opts$model){
-    # model training
-    library(caret)
-    library(broom)
-    library(glmnet)
-    library(lme4)
-    library(nlme)
-    library(lmerTest)
-  }
-
-  if(lib_opts$develop){
-    # development
-    library(optparse)
-    library(logging)
-  }
-
-}
-
+lib <- function(...) scriptR::lib(...)
