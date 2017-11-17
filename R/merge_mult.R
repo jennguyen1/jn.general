7377bbc99a6427da74d80939cc8cff7a16a1522a
Jenny <jennifernguyen1992@gmail.com>
2017-11-17 15:40:40 -0500

    fix bugs with merge function on example

--- R/merge_mult.R
+++ R/merge_mult.R
@@ -5,6 +5,7 @@
 #' @param by vector of shared column names to merge on or list of vectors where each element of list refers to the by value for the ith merge
 #' @param f merge function, valid options are dplyr::inner_join, dplyr::left_join, dplyr::right_join, dplyr::full_join, dplyr::semi_join, dplyr::anti_join, base::merge (default options)
 #' @param suffixes vector specifying suffixes to be used for making unique the names of columns in the result which are not used for merging
+#' 
 #' @return A data frame
 #'
 #' @export
@@ -14,26 +15,26 @@
 #' y1 <- data.frame(id = rep(1:5, 3), val = rnorm(15))
 #' y2 <- data.frame(id = 1:15, id2 = rep(c("A", "B", "C"), 5), est = rnorm(15))
 #' y3 <- data.frame(id = 1:15, id2 = rep(c("A", "B", "C"), 5), var = runif(15))
-#'
+#' 
 #' # merge by various attributes
-#' merge_mult(y1, y2, y3, by = list("id", c("id", "id2")))
+#' merge_mult(y1, y2, y3, f = dplyr::inner_join, by = list("id", c("id", "id2")), suffixes = letters[1:3])
 #' # equivalent to:
 #' merge(y1, y2, by = "id") %>% merge(y3, by = c("id", "id2"))
 
-merge_mult <- function(..., by, f, suffixes = letters){
+merge_mult <- function(..., f, by, suffixes = letters){
   l <- list(...)
   if( !is.data.frame(l[[1]]) ) l <- l[[1]] # TODO did they apply a second list?
 
   ## checks
-  assertthat::assert_that(all(lapply(l, is.data.frame)), msg = "Inputs should be data frames or a list of data frames")
+  assertthat::assert_that(all(unlist(lapply(l, is.data.frame))), msg = "Inputs should be data frames or a list of data frames")
   assertthat::assert_that(length(l) >= 2, msg = "At least 2 data frames must be supplied for merge")
-  assertthat::assert_that(!missing(by), msg = "Missing by argument")
   
   assertthat::assert_that(!missing(f), msg = "Missing f argument")
   merge_options <- c(dplyr::inner_join, dplyr::left_join, dplyr::right_join, dplyr::full_join, dplyr::semi_join, dplyr::anti_join, base::merge)
   valid_function <- any(unlist(lapply(merge_options, function(x) identical(x, f))))
   assertthat::assert_that(valid_function, msg = "Invalid merge function")
   
+  assertthat::assert_that(!missing(by), msg = "Missing by argument")
   assertthat::assert_that(length(suffixes) == length(l), msg = "The number of suffixes must be equal to the number of data frames")
   assertthat::assert_that((length(by) == 1) | (length(l) - length(by) == 1), msg = "Length of by argument must be either 1 or 1 less than the total data frames") # enough by keys for merge
 
@@ -43,9 +44,10 @@ merge_mult <- function(..., by, f, suffixes = letters){
   
   use_suffix <- paste0("_", suffixes)
   if(length(by) == 1) by <- rep(by, length(l)-1) # maintain consistency with a list of by vars
-
+  merged <- l[[1]]
+  
   if(length(l) == 2){
-    merged <- if(f_is_no_suffix_merge) f(l[[1]], l[[2]], by = by[1]) else f(l[[1]], l[[2]], by = by[1], suffix = use_suffix)
+    merged <- if(f_is_no_suffix_merge) f(l[[1]], l[[2]], by = by[[1]]) else f(l[[1]], l[[2]], by = by[[1]], suffix = use_suffix)
   } else{
     for(i in 2:length(l)){
       use_suffixes <- if(i == length(l)){ # not last merges
@@ -53,7 +55,7 @@ merge_mult <- function(..., by, f, suffixes = letters){
       } else{ # last merges
         use_suffix[(length(l)-1):length(l)]
       }
-      merged <- if(f_is_no_suffix_merge) f(l[[1]], l[[2]], by = by[1]) else f(l[[1]], l[[2]], by = by[1], suffix = use_suffixes)
+      merged <- if(f_is_no_suffix_merge) f(merged, l[[i]], by = by[[i - 1]]) else f(merged, l[[i]], by = by[[i - 1]], suffix = use_suffixes)
     }
   }
   return(merged)
