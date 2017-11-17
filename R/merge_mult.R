ae9d6b9a2916d95e7dd428671841652ec9e2f425
Jenny <jennifernguyen1992@gmail.com>
2017-11-17 15:12:14 -0500

    revamp mult_merge function

--- R/merge_mult.R
+++ R/merge_mult.R
@@ -1,34 +1,15 @@
 
 #' Merge Multiple Data Frames
 #'
-#' Fast merge multiple data frames, via data.table merge method
-#'
 #' @param ... data frames to be merged or one list of all data frames to be merged
 #' @param by vector of shared column names to merge on or list of vectors where each element of list refers to the by value for the ith merge
-#' @param all logical; if FALSE, then an inner join on all data frames; else a full outer join on all data frames.
-#' @param all.x logical; if TRUE, then a left outer join on the first data frame listed
+#' @param f merge function, valid options are dplyr::inner_join, dplyr::left_join, dplyr::right_join, dplyr::full_join, dplyr::semi_join, dplyr::anti_join, base::merge (default options)
 #' @param suffixes vector specifying suffixes to be used for making unique the names of columns in the result which are not used for merging
-#' @return A data table
+#' @return A data frame
 #'
 #' @export
 #'
 #' @examples
-#' # generate random data
-#' x1 <- rdata(n = 28, gen_random())
-#' x2 <- rdata(n = 11, gen_random())
-#' x3 <- rdata(n = 54, gen_random(2))
-#' x4 <- rdata(n = 9, gen_random())
-#'
-#' # merge by "id"
-#' merge_mult(x1, x2, x3, x4, by = "id")
-#' # equivalent to:
-#' merge(x1, x2, by = "id") %>% merge(x3, by = "id") %>% merge(x4, by = "id")
-#'
-#' # merge by "id", keep all of the 1st df
-#' merge_mult(x1, x2, x3, x4, by = "id", all.x = TRUE)
-#' # equivalent to:
-#' merge(x1, x2, by = "id", all.x = TRUE) %>% merge(x3, by = "id", all.x = TRUE) %>% merge(x4, by = "id", all.x = TRUE)
-#'
 #' # generate more random data
 #' y1 <- data.frame(id = rep(1:5, 3), val = rnorm(15))
 #' y2 <- data.frame(id = 1:15, id2 = rep(c("A", "B", "C"), 5), est = rnorm(15))
@@ -39,68 +20,41 @@
 #' # equivalent to:
 #' merge(y1, y2, by = "id") %>% merge(y3, by = c("id", "id2"))
 
-merge_mult <- function(..., by, all = FALSE, all.x = NULL, suffixes = letters){
-
-  ##################
-  # Error Checking #
-  ##################
-
-  # generate the list of data tables
+merge_mult <- function(..., by, f, suffixes = letters){
   l <- list(...)
-  if( !is.data.frame(l[[1]]) ) l <- l[[1]]
+  if( !is.data.frame(l[[1]]) ) l <- l[[1]] # TODO did they apply a second list?
 
-  # check that arguments were passed
-  if( length(l) < 2 ) stop("At least 2 data frames must be supplied for merge")
-  if( missing(by) ) stop("Missing by argument")
+  ## checks
+  assertthat::assert_that(all(lapply(l, is.data.frame)), msg = "Inputs should be data frames or a list of data frames")
+  assertthat::assert_that(length(l) >= 2, msg = "At least 2 data frames must be supplied for merge")
+  assertthat::assert_that(!missing(by), msg = "Missing by argument")
+  
+  assertthat::assert_that(!missing(f), msg = "Missing f argument")
+  merge_options <- c(dplyr::inner_join, dplyr::left_join, dplyr::right_join, dplyr::full_join, dplyr::semi_join, dplyr::anti_join, base::merge)
+  valid_function <- any(unlist(lapply(merge_options, function(x) identical(x, f))))
+  assertthat::assert_that(valid_function, msg = "Invalid merge function")
+  
+  assertthat::assert_that(length(suffixes) == length(l), msg = "The number of suffixes must be equal to the number of data frames")
+  assertthat::assert_that((length(by) == 1) | (length(l) - length(by) == 1), msg = "Length of by argument must be either 1 or 1 less than the total data frames") # enough by keys for merge
 
-  # check that there are enough suffixes for the list
-  if(length(suffixes) != length(l)) stop("The number of suffixes must be equal than the number of data frames")
+  ## function work
+  no_suffix_merges <- c(dplyr::semi_join, dplyr::anti_join)
+  f_is_no_suffix_merge <- any(unlist(lapply(no_suffix_merges, function(x) identical(x, f))))
+  
+  use_suffix <- paste0("_", suffixes)
+  if(length(by) == 1) by <- rep(by, length(l)-1) # maintain consistency with a list of by vars
 
-  # check that by is either 1 key or has enough keys for the merge
-  if( !((length(by) == 1) | (length(l) - length(by) == 1)) ) stop("Length of by argument must be either 1 or 1 less than the total data frames")
-
-  #################
-  # Function Work #
-  #################
-
-  # add prefix to suffixes
-  suffix <- paste0("_", suffixes)
-
-  # initalize the merge as a data.table
-  merged <- data.table::data.table(l[[1]])
-
-  # if there is 1 id for all merge, makes it a replicated list to ensure consistency
-  if(length(by) == 1) by <- rep(by, length(l)-1)
-
-  # note: must do all and all.x separately; doing them together in one call yields inconsistent results
-
-  # merge for two data frames
   if(length(l) == 2){
-
-    if(is.null(all.x)){
-      merged <- merge(l[[1]], l[[2]], by = by[[1]], all = all, suffix = suffix)
-    } else{
-      merged <- merge(l[[1]], l[[2]], by = by[[1]], all.x = all.x, suffix = suffix)
-    }
-
+    merged <- if(f_is_no_suffix_merge) f(l[[1]], l[[2]], by = by[1]) else f(l[[1]], l[[2]], by = by[1], suffix = use_suffix)
   } else{
-
-    # merge all the data frames via a for loop (faster than Reduce); maintain suffixes
-    for ( i in 2:length(l) ) {
-
-      # choose suffixes based on what is merging
-      suffixes <- ifelse( rep(i == length(l), 2), suffix[(length(l)-1):length(l)], c(suffix[(i-1)], ""))
-
-      if (is.null(all.x)) {
-        merged <- merge(merged, l[[i]], by = by[[i - 1]], all = all, suffix = suffixes)
+    for(i in 2:length(l)){
+      use_suffixes <- if(i == length(l)){ # not last merges
+        c(use_suffix[i-1], "")
+      } else{ # last merges
+        use_suffix[(length(l)-1):length(l)]
       }
-      else {
-        merged <- merge(merged, l[[i]], by = by[[i - 1]], all.x = all.x, suffix = suffixes)
-      }
+      merged <- if(f_is_no_suffix_merge) f(l[[1]], l[[2]], by = by[1]) else f(l[[1]], l[[2]], by = by[1], suffix = use_suffixes)
     }
-
   }
-
-  # return the results
   return(merged)
 }
