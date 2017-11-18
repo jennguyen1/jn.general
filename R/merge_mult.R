5a709e21805faa8a14ea04f609ad2d85dd42d944
Jenny Nguyen <jennifernguyen1992@gmail.com>
2017-11-17 21:41:52 -0500

    revamp merge_mult, don't need to rely on data table

--- R/merge_mult.R
+++ R/merge_mult.R
@@ -2,8 +2,8 @@
 #' Merge Multiple Data Frames
 #'
 #' @param ... data frames to be merged or one list of all data frames to be merged
-#' @param by vector of shared column names to merge on or list of vectors where each element of list refers to the by value for the ith merge
 #' @param f merge function, valid options are dplyr::inner_join, dplyr::left_join, dplyr::right_join, dplyr::full_join, dplyr::semi_join, dplyr::anti_join, base::merge (default options)
+#' @param by vector of shared column names to merge on or list of vectors where each element of list refers to the by value for the ith merge
 #' @param suffixes vector specifying suffixes to be used for making unique the names of columns in the result which are not used for merging
 #' 
 #' @return A data frame
@@ -20,41 +20,65 @@
 #' merge_mult(y1, y2, y3, f = dplyr::inner_join, by = list("id", c("id", "id2")), suffixes = letters[1:3])
 #' # equivalent to:
 #' merge(y1, y2, by = "id") %>% merge(y3, by = c("id", "id2"))
+#' 
 
-merge_mult <- function(..., f, by, suffixes = letters){
+merge_mult <- function(..., f, by, suffixes){
   l <- list(...)
-  if( !is.data.frame(l[[1]]) ) l <- l[[1]] # TODO did they apply a second list?
-
+  if(length(l) == 1) l <- l[[1]]
+  if(is.character(by)) by <- list(by)
+  merge_options <- c(dplyr::inner_join, dplyr::left_join, dplyr::right_join, dplyr::full_join, dplyr::semi_join, dplyr::anti_join, base::merge)
+  no_suffix_merges <- c(dplyr::semi_join, dplyr::anti_join)
+  f_is_no_suffix_merge <- any(unlist(lapply(no_suffix_merges, function(x) identical(x, f))))
+  
   ## checks
-  assertthat::assert_that(all(unlist(lapply(l, is.data.frame))), msg = "Inputs should be data frames or a list of data frames")
+  assertthat::assert_that(
+    is.list(l) & !is.data.frame(l),
+    all(unlist(lapply(l, is.data.frame))), 
+    msg = "Inputs should be data frames or a list of data frames"
+  )
   assertthat::assert_that(length(l) >= 2, msg = "At least 2 data frames must be supplied for merge")
   
   assertthat::assert_that(!missing(f), msg = "Missing f argument")
-  merge_options <- c(dplyr::inner_join, dplyr::left_join, dplyr::right_join, dplyr::full_join, dplyr::semi_join, dplyr::anti_join, base::merge)
   valid_function <- any(unlist(lapply(merge_options, function(x) identical(x, f))))
   assertthat::assert_that(valid_function, msg = "Invalid merge function")
   
   assertthat::assert_that(!missing(by), msg = "Missing by argument")
-  assertthat::assert_that(length(suffixes) == length(l), msg = "The number of suffixes must be equal to the number of data frames")
+  assertthat::assert_that(
+    (is.list(by) & !is.data.frame(by)) | is.character(by),
+    is.character(unlist(by)),
+    msg = "Invalid by argument"
+  )
   assertthat::assert_that((length(by) == 1) | (length(l) - length(by) == 1), msg = "Length of by argument must be either 1 or 1 less than the total data frames") # enough by keys for merge
 
+  assertthat::assert_that(!missing(suffixes) | f_is_no_suffix_merge, msg = "Missing suffixes argument")
+  if(missing(suffixes)) suffixes <- 1:length(l)
+  assertthat::assert_that(is.numeric(suffixes) | is.character(suffixes), msg = "Invalid suffixes argument")
+  assertthat::assert_that(length(suffixes) == length(l), msg = "The number of suffixes must be equal to the number of data frames")
+
   ## function work
-  no_suffix_merges <- c(dplyr::semi_join, dplyr::anti_join)
-  f_is_no_suffix_merge <- any(unlist(lapply(no_suffix_merges, function(x) identical(x, f))))
-  
   use_suffix <- paste0("_", suffixes)
-  if(length(by) == 1) by <- rep(by, length(l)-1) # maintain consistency with a list of by vars
+  if(length(by) == 1) by <- rep(by, length(l) - 1) # maintain consistency with a list of by vars
   merged <- l[[1]]
   
   if(length(l) == 2){
+    assertthat::assert_that(
+      all(by[[1]] %in% colnames(l[[1]])),
+      all(by[[1]] %in% colnames(l[[2]])),
+      msg = "by is not a valid column"
+    )
     merged <- if(f_is_no_suffix_merge) f(l[[1]], l[[2]], by = by[[1]]) else f(l[[1]], l[[2]], by = by[[1]], suffix = use_suffix)
   } else{
     for(i in 2:length(l)){
-      use_suffixes <- if(i == length(l)){ # not last merges
-        c(use_suffix[i-1], "")
-      } else{ # last merges
-        use_suffix[(length(l)-1):length(l)]
+      use_suffixes <- if(i == length(l)){ # last merges
+        use_suffix[(length(l) - 1):length(l)]
+      } else{ # not last merges
+        c(use_suffix[i - 1], "")
       }
+      assertthat::assert_that(
+        all(by[[i - 1]] %in% colnames(merged)),
+        all(by[[i - 1]] %in% colnames(l[[i]])),
+        msg = "by is not a valid column"
+      )
       merged <- if(f_is_no_suffix_merge) f(merged, l[[i]], by = by[[i - 1]]) else f(merged, l[[i]], by = by[[i - 1]], suffix = use_suffixes)
     }
   }
